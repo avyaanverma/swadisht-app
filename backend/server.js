3b24298a937451a01ea637787caa64e4c06ce7ce
avyaanverma <avyaanver@gmail.com>
2026-05-27 14:47:09 +0530

    morgan logging added, readme changed

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 // start server
-const server = require('./src/app')
+const app = require('./src/app')
 const connectDB = require('./src/db/db')
 
 connectDB();
 
-server.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
+app.listen(3000, ()=>{
+    console.log("Server is running on http://localhost:3000")
 })
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,23 +2,24 @@
 require('dotenv').config()
 const express = require("express");
 const cookieParser = require('cookie-parser')
+const morgan = require("morgan");
+const cors = require("cors")
 const authRoutes = require('./routes/auth.routes')
 const foodRoutes = require('./routes/food.routes')
-const foodPartnerRoutes = require("./routes/food-partner.routes")
+const reelsRoutes = require("./routes/reels.routes")
+const foodsRoutes = require("./routes/foods.routes")
+const foodPartnerRoutes = require("./routes/foodPartner.routes")
 
-const cors = require("cors")
 
 const app = express();
 
-// cookie parser ek middle ware hain jo humein cookies read karne meh help karta hain
 app.use(cookieParser());
-// express.json() ek middleware hain jo humein http bodies padhne meh help karta hain
 app.use(express.json());
-
 app.use(cors({
     origin:"http://localhost:5173",
     credentials: true
 }));
+app.use(morgan("tiny"));
 
 
 app.get('/' ,(req,res)=>{
@@ -26,7 +27,10 @@ app.get('/' ,(req,res)=>{
 })
 
 app.use('/api/auth' , authRoutes)
-app.use('/api/food', foodRoutes)
+// Backward-compat: old /api/food was used for reels earlier
+app.use("/api/food", foodRoutes)
+app.use("/api/reels", reelsRoutes)
+app.use("/api/foods", foodsRoutes)
 app.use('/api/food-partner', foodPartnerRoutes)
 
 module.exports = app
